ea2854f9737cfcc9ad66356b478025264173ade7
Javad Bayzavi <javadbayzavi@gmail.com>
2022-05-03 23:58:09 -0700

    Create datatypes.py

---
+++ datatypes.py
@@ -0,0 +1,15 @@
+tex = "We are in different"
+t = " part of the world"
+print(tex + t)
+
+#repeating output using multiple(*) operator
+print(tex + t * 4)
+
+#n = input("How many time do you like the text be repeated? ")
+#print("Hello world" * int(n))
+
+#slicing a string similar to slices in golang
+print(tex[1:3].capitalize())
+
+#Two form of defining sets
+
